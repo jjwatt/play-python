06e351a290dfc0d207a32fd1106e868f3826d1f5
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2020-09-25 11:33:23 -0400

    Add more codewars

--- my_alg_play/codewars.py
+++ my_alg_play/codewars.py
@@ -77,6 +77,77 @@ def delete_nthr(lst, N):
             return rec(tail)
     return list(reversed(rec(newlst)))
 
+# Array.diff Your goal in this kata is to implement a difference function,
+# which subtracts one list from another and returns the result.
+
+# It should remove all values from list a, which are present in list b.
+
+# array_diff([1,2],[1]) == [2]
+
+# If a value is present in b, all of its occurrences must be removed from the other:
+
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+
+def array_diff(l1, l2):
+    return [i for i in l1 if i not in l2]
+
+## Maximum subarray sum
+
+# The maximum sum subarray problem consists in finding the maximum sum of a
+# contiguous subsequence in an array or list of integers:
+
+# max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# # should be 6: [4, -1, 2, 1]
+
+# Easy case is when the list is made up of only positive numbers and the
+# maximum sum is the sum of the whole array. If the list is made up of only
+# negative numbers, return 0 instead.
+
+# Empty list is considered to have zero greatest sum. Note that the empty list
+# or array is also a valid sublist/subarray.
+
+def max_sequence(arr):
+    best_sum = 0
+    current_sum = 0
+    if not arr:
+        return 0
+    for i in arr:
+        current_sum = max(0, current_sum + i)
+        best_sum = max(best_sum, current_sum)
+    return best_sum
+
+## Take a Ten Minute Walk
+# You live in the city of Cartesia where all roads are laid out in a perfect
+# grid. You arrived ten minutes too early to an appointment, so you decided to
+# take the opportunity to go for a short walk. The city provides its citizens
+# with a Walk Generating App on their phones -- everytime you press the button
+# it sends you an array of one-letter strings representing directions to walk
+# (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each
+# letter (direction) and you know it takes you one minute to traverse one city
+# block, so create a function that will return true if the walk the app gives
+# you will take you exactly ten minutes (you don't want to be early or late!)
+# and will, of course, return you to your starting point. Return false
+# otherwise.
+
+#     Note: you will always receive a valid array containing a random
+#     assortment of direction letters ('n', 's', 'e', or 'w' only). It will
+#     never give you an empty array (that's not a walk, that's standing
+#     still!).
+
+def is_valid_walk(walk):
+    '''Returns True if the walk is valid.'''
+    rules = { 'n': (1,0), 's': (-1,0), 'e': (0,1), 'w': (0,-1) }
+    start = (0, 0)
+    loc = start
+    counter = 0
+    for i in walk:
+        loc = (loc[0] + rules[i][0], loc[1] + rules[i][1])
+        counter = counter + 1
+    return counter == 10 and loc == start
+
+def is_valid_walk_tr(walk):
+    return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
+
 import random
 # myrandom10list = random.choices(range(1, 50), k=10)
 def createlistofrandos(n, rng=None):
